3d4b2ccbd56cfafd74c711f3129d688ab917bb75
purzbeats <97489706+purzbeats@users.noreply.github.com>
2022-06-02 16:15:23 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,21 @@ export default {
   margin-top: 60px;
 }
 body {
-  background: linear-gradient(to bottom right, #f5f7fa  , #c3cfe2);
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  height: 100vh;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
 }
 </style>
