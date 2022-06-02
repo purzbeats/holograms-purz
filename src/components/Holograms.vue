f0ba932b9b4fb8aefef14b81ae72159c7caf2e05
purzbeats <97489706+purzbeats@users.noreply.github.com>
2022-06-02 14:01:33 -0400

    updates

--- src/components/Holograms.vue
+++ src/components/Holograms.vue
@@ -2,6 +2,56 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>Hello I'm Purz and these are my Holograms.</p>
+
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=676&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=675&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=672&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=671&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=631&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=627&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=620&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=619&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=608&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=589&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=588&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=587&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=586&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=578&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=577&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=572&initial_angle_pct=.9" frameborder="0"></iframe>
+    
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=539&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=534&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=532&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=526&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=525&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=524&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=523&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=518&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=488&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=461&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=449&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=442&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=441&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=440&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=435&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=433&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=430&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=429&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=413&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=376&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=370&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=367&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=334&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=333&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=329&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=320&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=319&initial_angle_pct=.9" frameborder="0"></iframe>
+    
+      <iframe class="wide" src="https://host.glass/embed/index.html?shortcode=614&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="wide" src="https://host.glass/embed/index.html?shortcode=423&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe class="wide" src="https://host.glass/embed/index.html?shortcode=416&initial_angle_pct=.9" frameborder="0"></iframe>
+  
   </div>
 </template>
 
