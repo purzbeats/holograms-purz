be4c49c44003b0d8158a214ecad2dad4bb5fa44f
purzbeats <97489706+purzbeats@users.noreply.github.com>
2022-06-02 14:07:24 -0400

    Update Holograms.vue

--- src/components/Holograms.vue
+++ src/components/Holograms.vue
@@ -3,8 +3,8 @@
     <h1>{{ msg }}</h1>
     <p>Hello I'm Purz and these are my Holograms.</p>
 
-      <iframe style="width: 400px;" src="https://host.glass/embed/index.html?shortcode=676&initial_angle_pct=.9" frameborder="0"></iframe>
-      <iframe style="width: 400px;" src="https://host.glass/embed/index.html?shortcode=675&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe style="width: auto; min-height: 50vh;" src="https://host.glass/embed/index.html?shortcode=676&initial_angle_pct=.9" frameborder="0"></iframe>
+      <iframe style="width: auto; min-height: 50vh;" src="https://host.glass/embed/index.html?shortcode=675&initial_angle_pct=.9" frameborder="0"></iframe>
       <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=672&initial_angle_pct=.9" frameborder="0"></iframe>
       <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=671&initial_angle_pct=.9" frameborder="0"></iframe>
       <iframe class="portrait" src="https://host.glass/embed/index.html?shortcode=631&initial_angle_pct=.9" frameborder="0"></iframe>
